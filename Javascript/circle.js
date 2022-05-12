b2d81b0b928aa61f76c6e5b9c354c64ade958f28
JeffreyChan <ngoyinchan@gmail.com>
2022-05-12 18:01:10 +0800

    finished circle

--- Javascript/circle.js
+++ Javascript/circle.js
@@ -1,67 +1,42 @@
 class DrawingCircle extends PaintFunction {
-    constructor(contextReal, contextDraft){
-        super();
-        this.contextReal = contextReal;    
-        this.contextDraft = contextDraft;      
-        this.escape = false;               
+    constructor(contextReal, contextDraft) {
+      super();
+      this.contextReal = contextReal;
+      this.contextDraft = contextDraft;
     }
-    
-    onMouseDown(coord, styleGuide, event){
-        this.escape = false;           
-        setCanvasToStyleGuide(2);
-        contextReal.setLineDash([]);
-        contextDraft.setLineDash([]);
-        this.origX = coord[0];
-        this.origY = coord[1];
+  
+    onMouseDown(coord) {
+      this.origX = coord[0];
+      this.origY = coord[1];
     }
-     
-    onDragging(coord, styleGuide, event){
-        $(document).keyup((e) => {
-            if(e.key == 'Escape') {
-                this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-                this.escape = true;
-            } 
-        });
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.checkAndDraw(this.origX, this.origY, coord[0], coord[1], this.contextDraft);
+  
+    onDragging(coord) {
+      this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+      this.contextDraft.beginPath();
+      this.contextDraft.arc(
+        this.origX,
+        this.origY,
+        Math.abs(this.origX - coord[0] + this.origY - coord[1]),
+        0,
+        Math.PI * 2
+      );
+      this.contextDraft.stroke();
+      this.contextDraft.fill();
     }
-
+  
     onMouseUp(coord) {
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.checkAndDraw(this.origX, this.origY, coord[0], coord[1], this.contextReal);
-        this.escape = false;
+      this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+      this.contextReal.beginPath();
+      this.contextReal.arc(
+        this.origX,
+        this.origY,
+        Math.abs(this.origX - coord[0] + this.origY - coord[1]),
+        0,
+        Math.PI * 2
+      );
+      this.contextReal.stroke();
+      this.contextReal.fill();
+      saveToCanvas();
     }
-
-    onMouseLeave(){
-        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        this.escape = true;
-    }
-
-    onMouseEnter(){}
-    onMouseMove(){}
-
-    checkAndDraw (x1, y1, x2, y2, context) {
-        if (this.escape == false) {
-            if (keyListeners.shift == false) {
-                this.drawEllipse(x1, y1, x2, y2, context);
-            } else {
-                this.drawCircle(x1, y1, x2, y2, context);
-            }
-            beforeDraw();
-        }
-    }
-
-    drawCircle (x1, y1, x2, y2, context) {
-        context.beginPath();
-        context.arc(x1, y1, Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2), 0, 2 * Math.PI);
-        context.stroke();
-        context.fill();
-    }
-
-    drawEllipse (x1, y1, x2, y2, context) {
-        context.beginPath();
-        context.ellipse(x1, y1, Math.abs(x2 - x1), Math.abs(y2 - y1), 0, 0, 2 * Math.PI);
-        context.stroke();
-        context.fill();
-    }
-}
+  }
+  
